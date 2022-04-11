af67df801a6d936a8c043ffa5c4167635d364529
GGG <kuranberitorappu@gmail.com>
2022-04-11 08:43:37 +0000

    確認画面作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,25 +1,24 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
 
-  # GET /pictures or /pictures.json
   def index
     @pictures = Picture.all
   end
 
-  # GET /pictures/1 or /pictures/1.json
   def show
   end
 
-  # GET /pictures/new
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
   end
 
-  # GET /pictures/1/edit
   def edit
   end
 
-  # POST /pictures or /pictures.json
   def create
     @picture = Picture.new(picture_params)
 
@@ -34,7 +33,6 @@ class PicturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pictures/1 or /pictures/1.json
   def update
     respond_to do |format|
       if @picture.update(picture_params)
@@ -47,10 +45,8 @@ class PicturesController < ApplicationController
     end
   end
 
-  # DELETE /pictures/1 or /pictures/1.json
   def destroy
     @picture.destroy
-
     respond_to do |format|
       format.html { redirect_to pictures_url, notice: "Picture was successfully destroyed." }
       format.json { head :no_content }
@@ -58,13 +54,11 @@ class PicturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_picture
       @picture = Picture.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def picture_params
-      params.require(:picture).permit(:image, :content)
+      params.require(:picture).permit(:image, :content, :image_cache)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :pictures
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+
 end
