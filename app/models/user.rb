38a421391baa3599bfa1603762cd339442935706
GGG <kuranberitorappu@gmail.com>
2022-04-12 02:30:04 +0000

    Userコントローラー作成newアクション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  before_validation { email.downcase! }
+  has_secure_password
 end
